5958ba4a90d71104fe31cde727b099a36f28539e
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-13 15:25:39 +0000

    Create ECR.tf

---
+++ terraform/ECR.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "cloud" {
+  name                 = "cloud-monitoring-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
