c0bc3507b80e2280a7cb0012379e4727a05a15d2
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-13 15:36:08 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,16 @@
+# ecs.tf
+
 module "ecs_app" {
   source                       = "./modules/ecs"
   ec2_task_execution_role_name = "EcsTaskExecutionRoleName"
   ecs_auto_scale_role_name     = "EcsAutoScaleRoleName"
-  app_image                    = "766261352911.dkr.ecr.us-west-2.amazonaws.com/cloud-monitoring-app
-  app_port                     = 5000
-  app_count                    = 1
+  app_image                    = "766261352911.dkr.ecr.us-west-2.amazonaws.com/cloud-monitoring-app"
+  app_port                     = "5000"
+  app_count                    = "1"
   health_check_path            = "/"
   fargate_cpu                  = "1024"
   fargate_memory               = "2048"
-  aws_region                   = us-west-2
+  aws_region                   = "us-west-2"
   az_count                     = "2"
   subnets                      = module.network.public_subnet_ids
   sg_ecs_tasks                 = [module.security.ecs_tasks_security_group_id]
@@ -30,6 +32,20 @@ module "security" {
 module "logs" {
   source            = "./modules/logs"
   log_group_name    = "/ecs/ecs-app"
+  log_stream_name   = "ecs-log-stream"
+  retention_in_days = 30
+}
+
+module "remote_backend" {
+  source              = "./modules/backend"
+  bucket_name         = "joel-terraform-state-backend"
+  dynamodb_table_name = "joel-terraform-state-lock-table"
+}
+
+module "s3" {
+  source = "./modules/s3_img"
+  bucket_name = "joel-bucket-image"
+}group_name    = "/ecs/ecs-app"
   log_stream_name   = "ecs-log-stream"
   retention_in_days = 30
 }
