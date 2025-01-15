f89ebcc98401f6b5eba32147838bc2fdf36791fd
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-15 14:20:08 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,25 @@
-variable "aws_region" {
-  default = "us-west-2"
+
+variable "region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-west-2"
 }
+
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where resources will be created"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs for the ECS service"
+  type        = list(string)
