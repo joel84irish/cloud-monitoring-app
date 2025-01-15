d92bc047fc39bc2bc992a8236cc189a4bad38c38
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-15 14:10:09 +0000

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -18,7 +18,7 @@ data "template_file" "ecs_app" {
 
 resource "aws_ecs_task_definition" "app" {
   family                   = "ecs-app-task"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.fargate_cpu
