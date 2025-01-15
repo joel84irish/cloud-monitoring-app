380f948b749aaa7977ddfe7d8fbe8c81e84971db
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-15 13:12:47 +0000

    Delete terraform/ecr.tf

--- terraform/ecr.tf
+++
@@ -1,8 +0,0 @@
-#resource "aws_ecr_repository" "cloud" {
-  name                 = "cloud-monitoring-app"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
