72e6339ded69a495750ef0577b5cf860b2e05690
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-17 23:38:30 +0000

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -26,7 +26,7 @@ resource "aws_ecs_service" "service" {
   name            = "monitoring_service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 1
+  desired_count   = 2
   launch_type     = "FARGATE"
 
   network_configuration {
